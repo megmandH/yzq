d6f03d5ee61a3b86e68e21bbf78f5f5ad5cc991a
megmandH <megmand@163.com>
2025-03-26 17:39:01 +0800

    修改主题

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,7 +10,7 @@ DEFAULT_LANG = 'zh_CN'
 
 OUTPUT_PATH = 'docs'
 
-# THEME = 'themes/alchemy'
+THEME = 'themes/notmyidea'
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -19,23 +19,25 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
+# LINKS = (
+#     ("Pelican", "https://getpelican.com/"),
+#     ("Python.org", "https://www.python.org/"),
+#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#     ("You can modify those links in your config file", "#"),
+# )
 
 # Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
+# SOCIAL = (
+#     ("You can add links in your config file", "#"),
+#     ("Another social link", "#"),
+# )
 
 DEFAULT_PAGINATION = False
 DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = False
 DELETE_OUTPUT_DIRECTORY = True
+OUTPUT_RETENTION = [".hg", ".git", ".bzr"]
+
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
