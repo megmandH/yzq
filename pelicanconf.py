7c7d704e30d22f08df4603d3641b19e32cd5be71
megmandH <megmand@163.com>
2025-03-28 17:49:56 +0800

    修改主题

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,7 +10,7 @@ DEFAULT_LANG = 'zh_CN'
 
 OUTPUT_PATH = 'docs'
 
-THEME = 'two-column@'
+THEME = 'simple'
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
